99a00c6b82daea3ac2abff9e204a3fa3f194ad0d
jamiep <jwrpettman@gmail.com>
2015-06-03 13:27:34 +0100

    adds grunt and minified js and css

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+        build: {
+            src: 'mylife.js',
+            dest: 'min/mylife.min.js'
+        }
+    },
+   sass: {
+      dist: {
+          options: {
+            style: 'compressed'
+          },
+          files: {
+            'min/mylife.css': 'css/mylife.scss'
+          }
+      }
+  }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify', 'sass']);
+
+};
